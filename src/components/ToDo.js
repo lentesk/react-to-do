63ec29581bd99b0a0522edcf82bacb359daf2014
Katie Lentes <katie.lentes@homechef.com>
2018-04-08 19:12:17 -0500

    Resolved merge conflict

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,13 +4,8 @@ import React, { Component } from 'react';
    render() {
     return (
       <li>
-<<<<<<< HEAD
-           <input type="checkbox" checked={ this.props.isCompleted } />
-           <span>{ this.props.description }</span>
-=======
         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
         <span>{ this.props.description }</span>
->>>>>>> Events
       </li>
     );
   }
